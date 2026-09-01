6761b7b78e044e414805f25601e4a29f46b20383
deris.o2024 <deris.omalley@mymail.champlain.edu>
2026-09-01 15:31:52 -0400

    Basic data example

--- webpage/frontend/src/pages/Results.tsx
+++ webpage/frontend/src/pages/Results.tsx
@@ -1,6 +1,34 @@
 import { CartesianGrid, Legend, Line, LineChart, XAxis, YAxis } from 'recharts';
 import '../styles/pages.css';
 
+const data = [
+    {
+      "name": "Jan",
+      "uv": 4000,
+      "pv": 2400
+    },
+    {
+      "name": "Feb",
+      "uv": 3000,
+      "pv": 1398
+    },
+    {
+      "name": "Mar",
+      "uv": 2000,
+      "pv": 9800
+    },
+    {
+      "name": "Apr",
+      "uv": 2780,
+      "pv": 3908
+    },
+    {
+      "name": "May",
+      "uv": 1890,
+      "pv": 4800
+    }
+]
+  
 const Results = () => {
     return (
         <div className="page_container">
@@ -15,7 +43,7 @@ const Results = () => {
                 {/* Probably just Recharts with some extra styles from Shadcn */}
                 <h2>Mondrian</h2>
 
-                <LineChart style={{ width: '100%', aspectRatio: 1.618, maxWidth: 800, margin: 'auto' }} responsive>
+                <LineChart style={{ width: '100%', aspectRatio: 1.618, maxWidth: 800, margin: 'auto' }} responsive data={data}>
                     <CartesianGrid strokeDasharray="5 5" />
                     <XAxis dataKey="name" />
                     <YAxis width="auto" />
@@ -25,7 +53,7 @@ const Results = () => {
                 </LineChart>
 
                 <h2>Incognito</h2>
-                <LineChart style={{ width: '100%', aspectRatio: 1.618, maxWidth: 800, margin: 'auto' }} responsive>
+                <LineChart style={{ width: '100%', aspectRatio: 1.618, maxWidth: 800, margin: 'auto' }} responsive data={data}>
                     <CartesianGrid strokeDasharray="5 5" />
                     <XAxis dataKey="name" />
                     <YAxis width="auto" />
